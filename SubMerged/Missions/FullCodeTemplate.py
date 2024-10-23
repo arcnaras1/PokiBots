5a14110e787cfdb73d413029ad4ce1598226cc10
Sid68743 <sid.s.vivek@gmail.com>
2024-10-22 19:32:44 -0700

    Upload merged prapti and siddu

--- SubMerged/Missions/FullCodeTemplate.py
+++ SubMerged/Missions/FullCodeTemplate.py
@@ -1,12 +1,11 @@
 from BASEROBOT import *
-from east_base_missions_linked import EAST2, EAST3, EAST4
-from west_base_missions import WEST1, WEST2, WEST3, WEST4, WEST5
+from east_base_missions_linked import EAST2, EAST3
+from west_base_missions import WEST1, WEST3, WEST4, WEST5, WEST6
 
 br = BaseRobot(Port.B, Port.E, Port.F, Port.A, 56, 130)
-RunOrder = [WEST1, WEST2, WEST3, WEST4, WEST5, EAST2, EAST3, EAST4]
+RunOrder = [WEST1, WEST3, WEST4, WEST5, EAST2, EAST3]
 #RunOrder = [lambda: br.hub.light.on(Color.BROWN), lambda: br.hub.light.on(Color.RED),lambda: br.hub.light.on(Color.GREEN)]
 mission_counter = 1
-br.hub.light.on(Color.BLUE)
 while True:
     if Button.RIGHT in br.hub.buttons.pressed():
         mission_counter += 1
@@ -19,9 +18,7 @@ while True:
         wait(250)
     
     if Button.BLUETOOTH in br.hub.buttons.pressed():
-        br.hub.light.on(Color.GREEN)
         RunOrder[mission_counter - 1](br)
-        br.hub.light.on(Color.BLUE)
     br.hub.display.number(mission_counter)
 """
 for k, mission_run in enumerate(RunOrder):
--- SubMerged/Missions/west_base_missions.py
+++ SubMerged/Missions/west_base_missions.py
@@ -22,29 +22,76 @@ def WEST1(br):
    br.straight(-400)
    while not any(prime_hub.buttons.pressed()):
        wait(1)
-   br.straight(25)
-   br.straight(-45)
+   br.straight(70)
+   br.straight(-75)
 
 # Mission 5
 def WEST2(br):
-    br.settings(straight_speed=250)
-    br.settings(straight_acceleration=500)
-    br.settings(turn_rate=150)
-    br.settings(turn_acceleration=400)
-    br.use_gyro(True)
-    br.reset()
-    
-    br.straight(-310-320, Stop.BRAKE)
-    br.turn(90, Stop.BRAKE)
-    br.straight(237.7, Stop.BRAKE)
-    br.turn (90.7)
-    br.straight (-177)
+   leftwheel.reset_angle(0)
+   rightwheel.reset_angle(0)
+   br.front_attachment.run_angle(1000,-20)
 
-    br.straight(177)
-    br.turn(-90.7)
-    br.straight(-237.7, Stop.BRAKE)
-    br.turn(-90, Stop.BRAKE)
-    br.straight(320+310, Stop.BRAKE)
+
+
+
+   br.straight(310, Stop.BRAKE)
+   br.turn(113.7, Stop.BRAKE)
+   br.straight (-75)
+   br.turn (57.7, Stop.BRAKE)
+   br.straight(-56)
+   ###FReeze####
+   br.straight(-400, Stop.BRAKE)
+   br.turn(27.7, Stop.BRAKE)
+   br.straight(380.7, Stop.BRAKE)
+
+
+
+
+   br.turn (-22.7)
+
+
+   br.straight (57)
+
+
+
+
+
+
+
+
+
+
+
+
+   ###~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+   br.straight(-100  , Stop.BRAKE)
+   ###~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+   #### FREEZE ###
+   br.front_attachment.run_angle(1000,75)
+   br.curve(100,20, Stop.BRAKE)
+   br.turn (11)
+   br.straight(100)
+   """
+   br.turn (-20)
+
+
+   br.straight (25)
+   br.turn (25)
+   """
+   hub.light.on(Color.RED)
+   br.straight (70)
+   """
+   br.turn (-40)
+   hub.light.on(Color.RED)
+
+
+   br.straight (180)
+   """
+   br.front_attachment.run_angle(1000,-80)
+   br.turn (120)
+   br.straight (300)
 
 # Mission 6
 def WEST3(br):
@@ -97,17 +144,17 @@ def WEST4(br):
    wait(100)
    
    ############# Mission 4 #############
-   br.turn(2, Stop.BRAKE)
+   br.turn(1.50, Stop.BRAKE)
    br.turn_attachment(attachment_motor, 500, 135)
-   br.straight(-64)
+   br.straight(-58)
    br.turn_attachment(attachment_motor, 1500, -135)
    wait(100)
    
    ############# Mission 2 #############
    br.straight(-8, Stop.BRAKE)
-   br.turn_attachment(attachment_motor, 1500, 130)
+   br.turn_attachment(attachment_motor, 1500, 80)
    br.straight(60, Stop.BRAKE)
-   br.turn(22, Stop.BRAKE)
+   br.turn(19, Stop.BRAKE)
    br.turn_attachment(attachment_motor, 1500, -130)
    br.straight(-43)
    br.turn_attachment(attachment_motor, 1500, 130)
@@ -116,16 +163,16 @@ def WEST4(br):
    wait(100)
    
    ############# Mission 3 #############
-   br.turn(38, Stop.BRAKE)
+   br.turn(33, Stop.BRAKE)
    br.straight(-30)
-   br.turn(58, Stop.BRAKE)
+   br.turn(65.25, Stop.BRAKE)
    br.straight(-38)
    br.turn_attachment(attachment_motor, 500, 135)
    br.turn_attachment(attachment_motor, 500, -135)
    wait(100)
    
    ############# Return to Base #############
-   br.turn(105, Stop.BRAKE)
+   br.turn(110, Stop.BRAKE)
    br.straight(-520, Stop.BRAKE)
 
 # Mission 1 part 2
@@ -147,3 +194,15 @@ def WEST5(br):
    br.straight(190, Stop.BRAKE)
    br.turn(8, Stop.BRAKE)
    br.straight(-220, Stop.BRAKE)
+
+   def WEST6(br):
+      br.straight(-322)
+      br.turn(92)
+      br.straight (-100 )
+
+      br.turn(21)
+
+
+      br.settings(straight_speed=67)
+      br.straight(-133)
+      br.straight(143)
