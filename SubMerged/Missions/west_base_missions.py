3c081a3336f0476e74178df083146c736bbd18e6
Sid68743 <sid.s.vivek@gmail.com>
2024-10-24 18:07:22 -0700

    Add files via upload

--- SubMerged/Missions/west_base_missions.py
+++ SubMerged/Missions/west_base_missions.py
@@ -3,60 +3,19 @@ from BASEROBOT import *
 # Set up all devices.
 prime_hub = PrimeHub()
 
-
-##
-def WEST0(br: BaseRobot):   
-    br.settings(straight_speed=200)
-    br.settings(straight_acceleration=400)
-    br.settings(turn_rate=110)
-    br.settings(turn_acceleration=400)
-    br.use_gyro(True)
-    
-    br.straight(322)
-    br.turn(92)
-    br.straight (100 )
-    br.turn(23)
-
-
-    br.settings(straight_speed=60)
-    br.straight(95)
-    br.straight(-143)   
-    
-    br.settings(straight_speed=60)
-    br.straight(95)
-    br.straight(-143) 
-
 # Collect corals and 1 Krill
 def WEST1(br):
-   br.straight(230)
-   br.straight(-300)
-   while not any(prime_hub.buttons.pressed()):
-       wait(1)
-   br.straight(320)
-   br.straight(-330)
-   while not any(prime_hub.buttons.pressed()):
-       wait(1)
-   br.straight(400)
-   br.straight(-400)
-   while not any(prime_hub.buttons.pressed()):
-       wait(1)
-   br.straight(70)
-   br.straight(-75)
+   #First Coral
+   br.straight(350)
+   br.straight(-370)
 
 # Mission 5
 def WEST2(br):
-   br.settings(straight_speed=150)
-br.settings(straight_acceleration=500)
-br.settings(turn_rate=50)
-br.settings(turn_acceleration=400)
-br.use_gyro(True)
-
-    
-    br.straight(-310-320, Stop.BRAKE)
+   br.straight(-310-320, Stop.BRAKE)
    br.turn(90, Stop.BRAKE)
    br.straight(237.7, Stop.BRAKE)
    br.turn (90.7)
-   br.straight (-177)
+   br.straight (-177, Stop.BRAKE)
 
    br.straight(177)
    br.turn(-90.7)
@@ -65,6 +24,8 @@ br.use_gyro(True)
    br.straight(320+310, Stop.BRAKE)
 # Mission 6
 def WEST3(br):
+
+
 
 
    br.straight(-322)
@@ -112,16 +73,16 @@ def WEST4(br):
    wait(100)
    
    ############# Mission 4 #############
-   br.turn(1.50, Stop.BRAKE)
+   br.turn(2.5, Stop.BRAKE)
    br.turn_attachment(attachment_motor, 500, 135)
-   br.straight(-58)
+   br.straight(-55)
    br.turn_attachment(attachment_motor, 1500, -135)
    wait(100)
    
    ############# Mission 2 #############
    br.straight(-8, Stop.BRAKE)
    br.turn_attachment(attachment_motor, 1500, 80)
-   br.straight(60, Stop.BRAKE)
+   br.straight(53, Stop.BRAKE)
    br.turn(19, Stop.BRAKE)
    br.turn_attachment(attachment_motor, 1500, -130)
    br.straight(-43)
@@ -131,9 +92,9 @@ def WEST4(br):
    wait(100)
    
    ############# Mission 3 #############
-   br.turn(33, Stop.BRAKE)
+   br.turn(30, Stop.BRAKE)
    br.straight(-30)
-   br.turn(65.25, Stop.BRAKE)
+   br.turn(60, Stop.BRAKE)
    br.straight(-38)
    br.turn_attachment(attachment_motor, 500, 135)
    br.turn_attachment(attachment_motor, 500, -135)
@@ -142,6 +103,8 @@ def WEST4(br):
    ############# Return to Base #############
    br.turn(110, Stop.BRAKE)
    br.straight(-520, Stop.BRAKE)
+    ############Return Seaweed#########
+   
 
 # Mission 1 part 2
 def WEST5(br):
