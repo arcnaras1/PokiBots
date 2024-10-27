870b3b6244f7a3a49f77fedd2ce8c4a1d6675e6d
Prady0918 <pradyuun.k.rishi@gmail.com>
2024-10-26 17:33:56 -0700

    Update FullCodeTemplate.py

--- SubMerged/Missions/FullCodeTemplate.py
+++ SubMerged/Missions/FullCodeTemplate.py
@@ -1,6 +1,6 @@
 from BASEROBOT import *
 from east_base_missions_linked import EAST1, EAST2, EAST3
-from west_base_missions import WEST1, WEST2, WEST3, WEST4, WEST5, WEST6
+from west_base_missions import WEST1, WEST2, WEST3, WEST4, WEST5
 
 br = BaseRobot(Port.B, Port.E, Port.F, Port.A, 56, 130)
 RunOrder = [WEST1, WEST2, WEST3, WEST4, WEST5, EAST1, EAST2, EAST3]
