18e96b7dcdc69d7aa6e31730edd372bfffb9f486
prapti <praptihpatil@gmail.com>
2025-02-23 18:53:22 -0800

    Update EAST_BASE_REGIONALS.py

--- SubMerged/Missions/EAST_BASE_REGIONALS.py
+++ SubMerged/Missions/EAST_BASE_REGIONALS.py
@@ -1,20 +1,9 @@
 if __name__ == "__main__":
     import FullCodeTemplate
 from BASEROBOT import *
-def EAST1(br: BaseRobot):
-    br.settings(straight_speed=200)
-    br.settings(straight_acceleration=200)
-    br.settings(turn_rate=150)
-    br.settings(turn_acceleration=400)
-    br.use_gyro(True)
-    br.hub.imu.reset_heading(0)
-    br.left_attachment.run_angle(400, -275)
-    wait(1000)
-    br.left_attachment.run_angle(400, 275)
 
-
-def EAST6(br):
-    br.settings(straight_speed=250)
+def EAST1(br):
+    br.settings(straight_speed=350)
     br.settings(straight_acceleration=500)
     br.settings(turn_rate=150)
     br.settings(turn_acceleration=400)
